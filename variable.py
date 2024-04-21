21175ff8b84c1d228f8a638c40384410c7d05366
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-21 08:23:15 +0530

    Create variable.py

---
+++ variable.py
@@ -0,0 +1,17 @@
+name="cristiano"
+age=42
+isgoal=True
+goalratio=1.23
+print(name)
+print(age)
+print(isgoal)
+print(goalratio)
+
+#assinging mutilple values to the variable
+a,b,c=1,2,3
+print(a,b,c)
+#single value to the mutiple variables 
+x=y=z= 7
+print(x)
+print(y)
+print(z)
