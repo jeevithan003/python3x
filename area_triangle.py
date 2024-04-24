c7605f600533fc1ee4fe639b8142f82e93583b0b
Jeevithan <117728163+jeevithan003@users.noreply.github.com>
2024-04-25 00:34:59 +0530

    Create area_triangle.py

---
+++ area_triangle.py
@@ -0,0 +1,4 @@
+# python program to calculate the area of the triagle
+length=float(input("enter the length\n"))
+breath=float(input("enter the breath\n"))
+print("the area of the triange is "+str((length*breath)/2))
